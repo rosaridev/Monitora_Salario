44265977579097da7a9ea45f79c74184e1e097bb
rosaridev <dev.rosarioo@gmail.com>
2025-11-07 10:09:04 -0300

    aprednedo usar CTK

--- teste_app_customtkinter.py
+++ teste_app_customtkinter.py
@@ -6,9 +6,29 @@ app= ctk.CTk()
 app.title('Divisor de salário')
 app.geometry('300x300')
 
+def validar_login():
+    usuario = campo_usuario.get()
+    senha = campo_senha.get()
+    
 
 
 
+
+#Label
+label_usuario = ctk.CTkLabel(app,text="Usuário")
+label_usuario.pack(pady=10)
+
+#entry
+campo_usuario = ctk.CTkEntry(app,placeholder_text="Digite seu Usuário")
+campo_usuario.pack(pady=10)
+
+label_senha = ctk.CTkLabel(app,text="senha")
+label_senha.pack(pady=10)
+
+campo_senha = ctk.CTkEntry(app,placeholder_text="Digite sua senha")
+campo_senha.pack(pady=10)
+
+ctk.CTkButton(app,text="Login", command=validar_login)
 
 
 app.mainloop()
