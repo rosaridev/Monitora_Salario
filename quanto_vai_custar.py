cc698834812703ab7e04a1924888d2cd72b5e932
rosaridev <dev.rosarioo@gmail.com>
2025-12-19 13:54:51 -0300

    fazendo alteração na porcentagem.

--- quanto_vai_custar.py
+++ quanto_vai_custar.py
@@ -26,7 +26,7 @@ horas_necessarias = produto/valor_hora
 dias_necessarios = horas_necessarias/8
 
 print("\n📊 RESULTADO:")
-print(f"O produto representa {porcentagem:.2f}% do seu salário.")
+print(f"O produto representa {porcentagem:.2f}% do seu salário.") #mudar para x% ou mais
 print(f"Você ganha aproximadamente R$ {valor_hora:.2f} por hora.")
 print(f"Você precisa trabalhar cerca de {horas_necessarias:.1f} horas para comprar o produto.")
 print(f"Isso equivale a aproximadamente {dias_necessarios:.1f} dias de trabalho.")
